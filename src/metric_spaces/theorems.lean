b3bc8f584bd3047421f6d5cbaea08bbfce1ef5de
JasonKYi <kexing.ying19@imperial.ac.uk>
2020-03-27 19:49:35 +0000

    added prod_continuous

--- src/metric_spaces/theorems.lean
+++ src/metric_spaces/theorems.lean
@@ -12,6 +12,8 @@ class metric_space (α : Type u) extends has_dist α : Type u :=
 -- Some of the theorems are already in mathlib so we will work in hidden
 namespace hidden
 
+noncomputable theory
+
 namespace examples
 
 -- Declaring X and element x with type X
@@ -19,7 +21,7 @@ inductive X : Type
 |   x : X
 
 -- A metric that maps everything to zero is a metric on X
-noncomputable instance metric_space_example_X : metric_space X :=
+instance metric_space_example_X : metric_space X :=
 {   dist := λ _ _, 0,
     dist_self := λ _, rfl,
     eq_of_dist_eq_zero := λ a b _, by {cases a, cases b, refl},
@@ -28,7 +30,7 @@ noncomputable instance metric_space_example_X : metric_space X :=
 }
 
 -- The absolute function is a metric on ℝ
-noncomputable instance metric_space_example_R : metric_space ℝ :=
+instance metric_space_example_R : metric_space ℝ :=
 {   dist := λ x y, abs (x - y),
     dist_self := λ x, show abs (x - x) = 0, by simp,
     eq_of_dist_eq_zero := λ x y, show abs (x - y) = 0 → x = y,
@@ -39,7 +41,6 @@ noncomputable instance metric_space_example_R : metric_space ℝ :=
 }
 
 -- The discrete metric is a metric on any set X
-noncomputable theory
 open tactic 
 open_locale classical
 
@@ -48,6 +49,7 @@ if x = y then 0 else 1
 
 variables {X : Type} {x y : X}
 
+-- Useless tactic just for fun, maybe remove later
 private meta def metric_example_tac : tactic unit :=
 solve1 $ intros
 >> `[unfold metric_example]
@@ -95,10 +97,10 @@ is_continuous (g ∘ f) := λ a ε hε,
     ⟨δ₂, hf₁, λ x hx, hg₂ (f x) (hf₂ x hx)⟩
 
 /- The product of two metric spaces is also a metric space -/
-instance : metric_space (X × Y) :=
+@[priority 20000] instance : metric_space (X × Y) :=
 {   dist := λ ⟨x₀, y₀⟩ ⟨x₁, y₁⟩, dist x₀ x₁ + dist y₀ y₁,
     dist_self := λ ⟨x, y⟩, show dist x x + dist y y = 0, by simp,
-    eq_of_dist_eq_zero := -- Why can't I use λ ⟨x₀, y₀⟩ ⟨x₁, y₁⟩ here?
+    eq_of_dist_eq_zero :=
         begin
             rintros ⟨x₀, y₀⟩ ⟨x₁, y₁⟩,
             show dist x₀ x₁ + dist y₀ y₁ = 0 → (x₀, y₀) = (x₁, y₁), intro h,
@@ -114,10 +116,26 @@ instance : metric_space (X × Y) :=
         by linarith [dist_triangle x₀ x₁ x₂, dist_triangle y₀ y₁ y₂]
 }
 
+theorem prod_continuous (f g : X → ℝ) (h₁ : is_continuous f) (h₂ : is_continuous g) : 
+is_continuous (λ x : X, (f x, g x)) := λ x₀ ε hε,
+    let ⟨δ₁, hδ₁, hf⟩ := h₁ x₀ (ε / 2) (half_pos hε) in
+    let ⟨δ₂, hδ₂, hg⟩ := h₂ x₀ (ε / 2) (half_pos hε) in
+    show ∃ (δ : ℝ), 0 < δ ∧ ∀ (x : X), dist x x₀ < δ → dist (f x) (f x₀) + dist (g x) (g x₀) < ε,
+    from ⟨min δ₁ δ₂, by simp; from ⟨hδ₁, hδ₂⟩, λ x hx,
+begin
+    suffices : dist (f x) (f x₀) < ε / 2 ∧ dist (g x) (g x₀) < ε / 2,
+        linarith [this.left, this.right],
+    split,
+        all_goals {apply hf x <|> apply hg x, apply lt_of_lt_of_le hx},
+        from inf_le_left, from inf_le_right
+end
+⟩
+
 /- TODO: now that we have the product of two metric spaces is also a metric space,
 we can show that (+) : ℝ × ℝ → ℝ is a continuous function and this the composition of 
 this and two continous functions f, g is also continuous, i.e. f + g is continuous.
 -/
 
 end continuity
+
 end hidden
