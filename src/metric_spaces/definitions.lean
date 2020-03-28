bc482be5a0be9117a16dc9e050bf534b8807d684
JasonKYi <kexing.ying19@imperial.ac.uk>
2020-03-28 15:26:44 +0000

    Definition of open ball and open

--- src/metric_spaces/definitions.lean
+++ src/metric_spaces/definitions.lean
@@ -3,6 +3,8 @@ import topology.metric_space.basic
 variables {X : Type*} [metric_space X]
 variables {Y : Type*} [metric_space Y]
 
+namespace definitions
+
 /- Definition of continuity on metric spaces -/
 def is_continuous_at (f : X → Y) (a : X) := ∀ ε > 0, ∃ δ > 0, ∀ x : X, dist x a < δ → dist (f x) (f a) < ε
 def is_continuous (f : X → Y) := ∀ a : X, is_continuous_at f a
@@ -12,3 +14,13 @@ def is_unif_continuous (f : X → Y) := ∀ ε > 0, ∃ δ > 0, ∀ x y : X, dis
 
 /- Notion of boundedness on metric spaces -/
 def is_bounded (S : set X) := S = ∅ ∨ ∃ x₀ ∈ S, ∃ k : ℝ, ∀ x ∈ S, dist x x₀ ≤ k
+
+/- Definition of an open ball -/
+def open_ball (x₀ : X) (r : ℝ) := {x : X | dist x₀ x < r}
+
+/- Definition of being open -/
+def is_open' (S : set X) := ∀ s ∈ S, ∃ ε : ℝ, open_ball s ε ⊆ S
+
+attribute [reducible] is_continuous is_continuous_at is_bounded open_ball is_open'
+
+end definitions
