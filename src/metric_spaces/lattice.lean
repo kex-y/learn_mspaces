770c78f2ad49febc44e3af9214227c9f5afb98aa
JasonKYi <kexing.ying19@imperial.ac.uk>
2020-04-05 21:05:58 +0100

    docs + tidy

--- src/metric_spaces/lattice.lean
+++ src/metric_spaces/lattice.lean
@@ -1,9 +1,19 @@
 import metric_spaces.theorems
 
+/--
+In this file we showed that the `closed_set` form a Galois insertion with
+`Closure` being its Galois connection, and with that, we conclude `closed_set`
+form a `complete_lattice`.
+
+- `closed_set` is defined to be a closed subset of a metric space
+- `Closure` is defined to be a `closed_set` with `closure' _` as its carrier
+-/
+
 variables {X : Type*} [metric_space X]
 variables {Y : Type*} [metric_space Y]
 
 open definitions
+open open_closed_sets
 
 namespace closed_set
 
@@ -14,43 +24,16 @@ structure closed_set (X : Type*) [metric_space X] :=
 
 instance : has_coe (closed_set X) (set X) := ⟨closed_set.carrier⟩
 
-open open_closed_sets
-
-/- The closure of a set is also closed -/
-lemma closure_closed (S : set X) : is_closed' $ closure' S :=
-Inter_closed_is_closed $ λ T, Inter_closed_is_closed $ λ h₁,
-Inter_closed_is_closed $ λ h₂, h₂
-
-/- The closure of a closure is itself -/
-theorem closure_closure' (S : set X) : closure' (closure' S) = closure' S := 
-closure_self $ closure_closed S
-
-/- The closure of a smaller set is smaller than closure -/
-theorem closure_mono' {S T : set X} (h : S ⊆ T) : closure' S ⊆ closure' T :=
-begin
-    iterate 2 {rw with_limit_points_is_closure},
-    suffices : limit_points S ⊆ limit_points T,
-        rw set.union_subset_iff,
-        split, refine set.subset.trans h _, simp,
-            refine set.subset.trans this _, simp,
-    from limit_points_le h
-end
-
-theorem monotone_closure' : monotone $ @closure' X _ := λ _ _, closure_mono'
-
-/- A set is smaller than its closure -/
-theorem subset_closure' (S : set X) : S ⊆ closure' S :=
-set.subset_Inter $ λ _, set.subset_Inter $ λ h, set.subset_Inter $ λ _, h
-
 def Closure (S : set X) : closed_set X := 
-{ carrier := closure' S,
-  is_closed := closure_closed S }
+{   carrier := closure' S,
+    is_closed := closure_closed S }
 
 theorem ext' {S T : closed_set X} (h : (S : set X) = T) : S = T :=
 by cases S; cases T; congr'
 
 open set
 
+/- Closed sets form a partial order -/
 instance : partial_order (closed_set X) :=
 {.. partial_order.lift (coe : closed_set X → set X) (λ a b, ext') (by apply_instance)}
 
@@ -60,11 +43,11 @@ ext' $ show ↑T = closure' T.carrier, by {rw closure_self, refl, from T.2}
 
 /- Closed sets form a Galois insertion -/
 def gi : @galois_insertion (set X) (closed_set X) _ _ Closure closed_set.carrier := 
-{ choice := λ S h, Closure S,
-  gc := λ S T, 
-    ⟨λ h, set.subset.trans (subset_closure' S) h, λ h, by rw Closure_self T; from closure_mono' h⟩,
-  le_l_u := λ S, subset_closure' S,
-  choice_eq := λ _ _, rfl }
+{   choice := λ S h, Closure S,
+    gc := λ S T, 
+        ⟨λ h, set.subset.trans subset_closure' h, λ h, by rw Closure_self T; from closure_mono' h⟩,
+    le_l_u := λ S, subset_closure',
+    choice_eq := λ _ _, rfl }
 
 /- Closed sets form a complete lattice -/
 instance : complete_lattice (closed_set X) := 
