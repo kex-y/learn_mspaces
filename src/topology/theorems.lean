f50b8e5f766bce3560e2d1e0808dbb2fab5e0463
Kexing <kexing.ying19@imperial.ac.uk>
2020-05-23 22:34:30 +0200

    propositions regarding closures espeically regarding orders

--- src/topology/theorems.lean
+++ src/topology/theorems.lean
@@ -51,7 +51,8 @@ lemma open_of_has_smaller {U : set X}
 is_open U :=
 begin
   choose f hfo hf using h,
-  have : is_open ⋃ (x ∈ U), f x H := is_open_Union (λ x, is_open_Union $ λ h, hfo x h), 
+  have : is_open ⋃ (x ∈ U), f x H := 
+    is_open_Union (λ x, is_open_Union $ λ h, hfo x h), 
   convert this, ext, 
   refine ⟨λ h, mem_Union.2 ⟨x, mem_Union.2 ⟨h, (hf x h).1⟩⟩, λ h, _⟩,
     cases mem_Union.1 h with y hy, cases mem_Union.1 hy with hy₀ hy₁,
@@ -75,8 +76,8 @@ is_continuous (g ∘ f) := λ U hU, hf _ (hg _ hU)
 lemma contin_at_all_of_contin {f : X → Y} (h : is_continuous f) : 
 ∀ x : X, is_continuous_at f x := λ _ U _ hU, h U hU
 
-lemma contin_of_contin_at_all {f : X → Y} (h : ∀ x : X, is_continuous_at f x) : 
-is_continuous f := λ U hU,
+lemma contin_of_contin_at_all {f : X → Y} 
+(h : ∀ x : X, is_continuous_at f x) : is_continuous f := λ U hU,
 begin
   cases (classical.em $ f ⁻¹' U = ∅) with hempt hnempt,
     { rw hempt, exact empty_is_open },
@@ -121,8 +122,9 @@ begin
     }
 end
 
-noncomputable theorem equiv_of_topo_contin_biject {f : X → Y} (hf₀ : bijective f) 
-(hf₁ : ∀ U : set X, is_open U → is_open (f '' U)) (hf₂ : is_continuous f) : X ≃* Y :=
+noncomputable theorem equiv_of_topo_contin_biject {f : X → Y} 
+(hf₀ : bijective f) (hf₁ : ∀ U : set X, is_open U → is_open (f '' U)) 
+(hf₂ : is_continuous f) : X ≃* Y :=
 { contin := hf₂,
   inv_contin := λ U hU, by rw ←preimage_eq_inv hf₀; exact hf₁ U hU,
   .. of_bijective hf₀ }
@@ -131,22 +133,25 @@ end mapping
 
 namespace closed
 
+/- Giving closure the attribute reducible so lean will automatically 
+unfold the definition instead of us manually telling it do so -/
+attribute [reducible] closure
+
+/- The closure of a set is the smallest closed set continaing it -/
+theorem closure_is_min {U U' : set X} (hle : U ⊆ U') (hc : is_closed U') :
+closure U ⊆ U' := 
+begin
+  intros x hx, rw mem_sInter at hx,
+  exact hx U' ⟨hc, hle⟩
+end
+
 /- The closure of a set is the set of limit points -/
 lemma limit_points_is_closed {U : set X}: 
 is_closed $ limit_points U := 
 begin
-  unfold is_closed,
   refine open_iff_has_smaller.2 (λ x hx, _),
   simp at hx, rcases hx with ⟨U', hU'₀, hU'₁, hU'₂⟩,
   exact ⟨U', hU'₀, hU'₁, λ y hy, by simp; exact ⟨U', hU'₀, hy, hU'₂⟩⟩,
-end
-
-lemma closure_is_min {U U' : set X} (hle : U ⊆ U') (hc : is_closed U') :
-closure U ⊆ U' := 
-begin
-  unfold closure, 
-  intros x hx, rw mem_sInter at hx,
-  exact hx U' ⟨hc, hle⟩
 end
 
 lemma limit_points_ge {U : set X} : U ⊆ limit_points U := 
@@ -165,5 +170,32 @@ not_subset.2 ⟨y, hy.2, hy.1⟩ hU'.2
 theorem closure_eq_limit_points (U : set X) : 
 closure U = limit_points U :=
 le_antisymm (closure_le_limit_points U) (limit_points_le_closure U)
+
+/- A set is smaller than its closure -/
+theorem set_le_closure (U : set X) : U ⊆ closure U := 
+λ x hx, mem_sInter.1 $ λ U' hU', hU'.2 hx
+
+/- If A ⊆ B then the closure of A is smaller than the closure of B -/
+theorem closure_mono (U V : set X) (hle : U ⊆ V) :
+closure U ⊆ closure V := λ x hx A hA, hx _ ⟨hA.1, subset.trans hle hA.2⟩
+
+/- The closure of a closed set is itself-/
+theorem closure_of_closed (U : set X) (h : is_closed U) :
+closure U = U := ext $ λ x, 
+  ⟨λ hx, hx U ⟨h, subset.refl U⟩, λ hx, set_le_closure U hx⟩
+
+/- The intersection of closed sets is closed -/
+theorem is_closed_Inter {ι} {f : ι → set X} (hf : ∀ i : ι, is_closed (f i)) : 
+is_closed (⋂ ι, f ι) :=
+by unfold is_closed; rw compl_Inter; refine is_open_Union hf
+
+/- The closure of a set is closed -/
+theorem closure_is_closed (U : set X) : is_closed $ closure U := 
+begin
+  unfold is_closed, rw compl_sInter,
+  refine is_open_sUnion (λ _ hU', _),
+  rcases hU' with ⟨_, ⟨hU'₀, _⟩, hU'₁⟩,
+  exact hU'₁ ▸ hU'₀
+end
 
 end closed
