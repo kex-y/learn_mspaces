e1828318357ad3c12cf5fc1c24cc0afb52e9e064
Kexing <kexing.ying19@imperial.ac.uk>
2020-05-24 16:44:09 +0200

    initial commit

---
+++ src/topology/exercies.lean
@@ -0,0 +1,19 @@
+import topology.theorems
+
+/-
+A topological space (X, 𝒯) consists of a non-empty set X 
+together with a collection 𝒯 of subsets of X that satisfy 
+- ∅ ∈ 𝒯, X ∈ 𝒯
+- U, V ∈ 𝒯 → U ∩ V ∈ 𝒯 
+- Uᵢ ∈ 𝒯 → ⋃ᵢ U ∈ 𝒯
+Elements of 𝒯 are called open sets in (X, 𝒯) and 𝒯 is 
+called a topology on X.
+
+In Lean this is represented by:
+
+structure topological_space (α : Type u) :=
+(is_open        : set α → Prop)
+(is_open_univ   : is_open univ)
+(is_open_inter  : ∀s t, is_open s → is_open t → is_open (s ∩ t))
+(is_open_sUnion : ∀s, (∀ t ∈ s, is_open t) → is_open (⋃₀ s))
+-/
