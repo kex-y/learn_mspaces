150b8ae6baef714e7d5a80b70ead0cea9bf222af
Kexing <kexing.ying19@imperial.ac.uk>
2020-06-17 18:22:38 +0200

    finished `connected_of_const_func`

--- src/metric_spaces/theorems.lean
+++ src/metric_spaces/theorems.lean
@@ -578,7 +578,40 @@ namespace connected
 inductive type binary containing terms: val_a and val_b -/
 inductive binary : Type* | val_a : binary | val_b : binary
 
-instance metric_space_of_zero_one : metric_space binary := sorry
+/- We need to show that binary form a metric space -/
+private def binary_metric : binary → binary → ℝ :=
+	λ x y, if (x = y) then 0 else 1
+
+private lemma binary_dist_self : ∀ x, binary_metric x x = 0 :=
+λ x, by unfold binary_metric; simp
+
+private lemma binary_eq_of_dist_eq_zero : 
+	∀ x y, binary_metric x y = 0 → x = y := λ x y h,
+begin
+	cases x; cases y;
+	try { refl <|>
+		{	exfalso, apply @zero_ne_one ℝ _, rw ←h,
+			unfold binary_metric, simp } }
+end
+
+private lemma binary_dist_comm : 
+	∀ x y, binary_metric x y = binary_metric y x := λ x y,
+by cases x; cases y; try { unfold binary_metric, simp }; refl
+
+private lemma binary_dist_triangle :
+	∀ x y z, binary_metric x z ≤ binary_metric x y + binary_metric y z := λ x y z,
+begin
+	cases x; cases y; cases z;
+	try {	unfold binary_metric, simp };
+	norm_num
+end
+
+instance metric_space_of_zero_one : metric_space binary := 
+{ dist := binary_metric, 
+  dist_self := binary_dist_self,
+  eq_of_dist_eq_zero := binary_eq_of_dist_eq_zero,
+  dist_comm := binary_dist_comm,
+  dist_triangle := binary_dist_triangle }
 
 lemma notempty {S : set α} (h : S ≠ ∅) : ∃ s : α, s ∈ S := 
 begin
@@ -586,13 +619,43 @@ begin
   exact (push_neg.not_eq _ _).1 h (set.eq_empty_iff_forall_not_mem.2 hs)
 end
 
+private def aux_fun (U : set X) : X → binary :=  
+	λ x, if (x ∈ U) then binary.val_a else binary.val_b
+
+private lemma aux_fun_eq_val_a {U : set X} : ∀ u ∈ U, aux_fun U u = binary.val_a :=
+λ u hu, by unfold aux_fun; split_ifs; refl	
+
+private lemma aux_fun_eq_val_b {U : set X} : ∀ u ∉ U, aux_fun U u = binary.val_b :=
+λ u hu, by unfold aux_fun; split_ifs; refl	
+
+lemma disj_if_inter_empty {U V : set X} (hdisj : U ∩ V = ∅) : 
+∀ v ∈ V, v ∉ U := λ v hv hu, by rw [←(mem_empty_eq v), ←hdisj]; exact ⟨hu, hv⟩
+
+private lemma aux_fun_is_continuous {U V : set X} 
+(hU₀ : is_open' U) (hV₀ : is_open' V) (hU₁ : U ≠ ∅) (hV₁ : V ≠ ∅) 
+(hdisj : U ∩ V = ∅) (hcover : U ∪ V = univ) : is_continuous (aux_fun U) := 
+begin
+	intros x ε hε, have := mem_univ x, 
+	rw ←hcover at this, cases this,
+		{	rcases hU₀ _ this with ⟨δ, hδ₀, hδ₁⟩,
+			refine ⟨δ, hδ₀, λ y hy, _⟩, convert hε,
+			have hyU : y ∈ U, apply hδ₁, rw dist_comm at hy, exact hy,
+			rw aux_fun_eq_val_a _ this, 
+			rw aux_fun_eq_val_a _ hyU,
+			exact dist_self _ },
+		{ rcases hV₀ _ this with ⟨δ, hδ₀, hδ₁⟩,
+			refine ⟨δ, hδ₀, λ y hy, _⟩, convert hε,
+			have hyV : y ∈ V, apply hδ₁, rw dist_comm at hy, exact hy,
+			rw aux_fun_eq_val_b x (disj_if_inter_empty hdisj _ this),
+			rw aux_fun_eq_val_b y (disj_if_inter_empty hdisj _ hyV),
+			exact dist_self _	}
+end
+
 lemma connected_of_const_func (h : ∀ f : X → binary, is_continuous f → 
   (f = λ _, binary.val_a) ∨ (f = λ _, binary.val_b)) : is_connected' X :=
 begin
 	rintro ⟨U, V, hU₀, hV₀, hU₁, hV₁, hdisj, hcover⟩,
-	let f : X → binary := 
-		λ x, if (x ∈ U) then binary.val_a else binary.val_b,
-	have := h f _,
+	have := h (aux_fun U) _,
 	cases notempty hU₁ with u hu,	cases notempty hV₁ with v hv,
 	apply (show binary.val_a ≠ binary.val_b, by finish),
 	cases this; rw function.funext_iff at this, 
@@ -603,12 +666,7 @@ begin
 		{ rw ←(this u),
 			show binary.val_a = (λ (x : X), ite (x ∈ U) binary.val_a binary.val_b) u,
 			simp, split_ifs, refl },
-	intros x ε hε,
-	have := mem_univ x, rw ←hcover at this, cases this,
-		{	rcases hU₀ _ this with ⟨δ, hδ₀, hδ₁⟩,
-			refine ⟨δ, hδ₀, λ y hy, _⟩,
-			convert hε, sorry },
-		sorry,
+	exact aux_fun_is_continuous hU₀ hV₀ hU₁ hV₁ hdisj hcover
 end
 
 end connected
